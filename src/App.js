285fb6aed959ab72f2e428931aff02d4e0be28f2
torvic <130111@unsaac.edu.pe>
2021-11-22 16:14:10 -0500

    Added repeated words functionality

--- src/App.js
+++ src/App.js
@@ -2,12 +2,21 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			data: ''
+		}
+	}
+	handleChange = (e) => {
+		this.setState({ data: e.target.value })
+	}
   render() {
     return (
       <div>
         <h1>Un repetidor</h1>
-        <input type="text" placeholder="Empieza a escribir algo" />
-        <p className="repeater"></p>
+        <input onChange={this.handleChange} type="text" placeholder="Empieza a escribir algo"/>
+        <p className="repeater">{this.state.data}</p>
       </div>
     );
   }
